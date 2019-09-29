e7bd769eb0edec9ff56dbf67e99b573ceacc300b
GangatharanRamu <Vishvaganga@apples-macbook-pro.local>
2019-09-29 15:19:40 +0530

    Commit

---
+++ GitSampletutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitSampletutorial
+//
+//  Created by GangatharanRamu on 29/09/19.
+//  Copyright © 2019 APPLE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
